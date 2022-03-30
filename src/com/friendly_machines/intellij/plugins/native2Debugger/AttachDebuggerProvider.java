02adb4d4358e2bb8c3da25df7301099d73f25dbf
Danny Milosavljevic <dannym@scratchpost.org>
2022-03-30 15:59:16 +0200

    Update copyright

--- src/com/friendly_machines/intellij/plugins/native2Debugger/AttachDebuggerProvider.java
+++ src/com/friendly_machines/intellij/plugins/native2Debugger/AttachDebuggerProvider.java
@@ -1,3 +1,5 @@
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the COPYING file.
+
 package com.friendly_machines.intellij.plugins.native2Debugger;
 
 import com.intellij.execution.process.ProcessInfo;
--- src/com/friendly_machines/intellij/plugins/native2Debugger/DebuggerBundle.java
+++ src/com/friendly_machines/intellij/plugins/native2Debugger/DebuggerBundle.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the COPYING file.
 package com.friendly_machines.intellij.plugins.native2Debugger;
 
 import com.intellij.DynamicBundle;

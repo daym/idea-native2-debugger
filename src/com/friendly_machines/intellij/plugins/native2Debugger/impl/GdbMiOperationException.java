59200175c759b499171673f2bf349efb71f731d0
Danny Milosavljevic <dannym@scratchpost.org>
2022-03-30 15:59:54 +0200

    Update copyright

--- src/com/friendly_machines/intellij/plugins/native2Debugger/impl/GdbMiOperationException.java
+++ src/com/friendly_machines/intellij/plugins/native2Debugger/impl/GdbMiOperationException.java
@@ -1,3 +1,5 @@
+// Copyright 2022 Danny Milosavljevic. Use of this source code is governed by the Apache 2.0 license that can be found in the COPYING file.
+
 package com.friendly_machines.intellij.plugins.native2Debugger.impl;
 
 public class GdbMiOperationException extends Exception {

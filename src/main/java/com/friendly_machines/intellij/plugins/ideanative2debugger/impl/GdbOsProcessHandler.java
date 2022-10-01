44c794178d0f131997fecd7b92254286a58c60fd
Danny Milosavljevic <daym@oxidecomputer.com>
2022-10-01 03:02:45 +0200

    Factor out and fix startDebugging.

--- src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/impl/GdbOsProcessHandler.java
+++ src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/impl/GdbOsProcessHandler.java
@@ -3,6 +3,9 @@ package com.friendly_machines.intellij.plugins.ideanative2debugger.impl;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.openapi.util.Key;
 import com.intellij.util.io.BaseDataReader;
 import com.intellij.util.io.BaseOutputReader;
@@ -67,19 +70,55 @@ public class GdbOsProcessHandler extends OSProcessHandler {
         return StandardCharsets.UTF_8;
     }
 
+    // This is here so we don't have an ordering problem with the startNotified events
+    public void startNotify() {
+        if (myCommandLine != null) {
+            notifyTextAvailable(myCommandLine + '\n', ProcessOutputTypes.SYSTEM);
+        }
+
+        addProcessListener(new ProcessAdapter() {
+            @Override
+            public void startNotified(@NotNull final ProcessEvent event) {
+                var p = (DebugProcess) GdbOsProcessHandler.this.getUserData(DebugProcess.DEBUG_PROCESS_KEY);
+                try {
+                    BaseDataReader stdOutReader = createOutputDataReader();
+                    BaseDataReader stdErrReader = processHasSeparateErrorStream() ? createErrorDataReader() : null;
+                    p.startDebugging();
+
+                    myWaitFor.setTerminationCallback(exitCode -> {
+                        try {
+                            // tell readers that no more attempts to read process' output should be made
+                            if (stdErrReader != null) stdErrReader.stop();
+                            stdOutReader.stop();
+
+                            try {
+                                if (stdErrReader != null) stdErrReader.waitFor();
+                                stdOutReader.waitFor();
+                            }
+                            catch (InterruptedException ignore) { }
+                        }
+                        finally {
+                            onOSProcessTerminated(exitCode);
+                        }
+                    });
+                }
+                finally {
+                    removeProcessListener(this);
+                }
+            }
+        });
+
+        super.startNotify();
+    }
+
     @Override
     public void notifyTextAvailable(@NotNull String text, @NotNull Key outputType) {
-        try {
-            throw new RuntimeException("e");
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-        System.err.println( Thread.currentThread().getId() + Thread.currentThread().getName() + " text available: " + text);
+        System.err.println(Thread.currentThread().getId() + Thread.currentThread().getName() + " notifyTextAvailable: " + text);
         System.err.flush();
-        if (text.startsWith("^")) { // sync response
-            var scanner = new Scanner(text);
-            scanner.useDelimiter(""); // character by character mode
-            Optional<String> token = GdbMiProducer.parseToken(scanner);
+        var scanner = new Scanner(text);
+        scanner.useDelimiter(""); // character by character mode
+        Optional<String> token = GdbMiProducer.parseToken(scanner);
+        if (scanner.hasNext("\\^")) { // sync response
             if (token.isPresent()) {
                 var item = GdbMiStateResponse.decode(token, scanner);
                 try {

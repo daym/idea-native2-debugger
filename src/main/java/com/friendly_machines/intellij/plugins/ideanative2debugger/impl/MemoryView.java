4576d4316039ad3dbcb48edb6a4aa8304b1ddaa0
Danny Milosavljevic <daym@oxidecomputer.com>
2023-02-18 01:40:06 +0100

    Implement MemoryView a little.

--- src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/impl/MemoryView.java
+++ src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/impl/MemoryView.java
@@ -5,15 +5,25 @@ import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.memory.component.InstancesTracker;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MemoryView extends BorderLayoutPanel {
     private final DebugProcess myProcess;
-    private JTextField textField1;
+    private JTextField beginningAddressText;
     private JPanel panel1;
+    private JButton viewButton;
+    private JTextArea memoryText;
+    private JPanel panel2;
 
 
     public JComponent getDefaultFocusedComponent() {
-        return null;
+        return beginningAddressText;
     }
 
     public void setActive(boolean value) {
@@ -21,11 +31,56 @@ public class MemoryView extends BorderLayoutPanel {
     }
     public MemoryView(XDebugSession session, DebugProcess process, InstancesTracker tracker) {
         myProcess = process;
-        this.add(panel1);
+        this.add(panel2);
+        //panel2.setDefaultButton(viewButton);
         /*
         TODO: [address|symbol] -Read->  [    ]
                                <-Write-
          */
+        viewButton.addActionListener(new ActionListener() {
+            Pattern partRegex = Pattern.compile(".{1,8}", Pattern.DOTALL);
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                var beginningA = beginningAddressText.getText();
+                try {
+                    // TODO: incrementally increase count.
+                    var response = (Map) myProcess.dataReadMemoryBytes(0, beginningA, 0x100);
+                    memoryText.setText("");
+                    var memory = (List) response.get("memory");
+                    for (var itemx: memory) {
+                        var item = (Map) itemx;
+                        var offset = item.get("offset");
+                        var contents = (String) item.get("contents");
+                        var begin = item.get("begin");
+                        // TODO: memoryText.setText(response.toString());
+                        memoryText.append("\n");
+                        memoryText.append("at " + begin.toString() + " + " + offset.toString() + ":\n  ");
+
+                        int chunkIndex = 0;
+                        var m = partRegex.matcher(contents);
+                        while (m.find()) {
+                            var part = contents.substring(m.start(), m.end());
+                            memoryText.append(part);
+                            memoryText.append(" ");
+                            ++chunkIndex;
+                            if ((chunkIndex & 3) == 0) {
+                                memoryText.append("\n  ");
+                            }
+                        }
+                        memoryText.append(contents);
+                    }
+                } catch (GdbMiOperationException e) {
+                    // TODO error throw new RuntimeException(e);
+                    process.reportError("failed reading memory", e);
+                } catch (IOException e) {
+                    // TODO error throw new RuntimeException(e);
+                    process.reportError(e.toString());
+                } catch (InterruptedException e) {
+                    // probably on purpose
+                }
+            }
+        });
     }
 
     /*

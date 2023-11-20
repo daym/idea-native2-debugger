e203a700402a02995a3718c820a1eab873312302
Danny Milosavljevic <dannym@scratchpost.org>
2023-11-20 19:23:46 +0100

    ProjectDebugSettingsEditorComponent: Clean up field names.

--- src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/ProjectDebugSettingsEditorComponent.java
+++ src/main/java/com/friendly_machines/intellij/plugins/ideanative2debugger/ProjectDebugSettingsEditorComponent.java
@@ -11,11 +11,11 @@ import java.util.Comparator;
 
 public class ProjectDebugSettingsEditorComponent implements PanelWithAnchor {
     private final DefaultTableModel myArgs;
-    private JPanel p;
+    private JPanel panel;
     private JButton addBeforeButton;
-    private JTable table1;
+    private JTable execArgumentsTable;
     private JButton addAfterwardsButton;
-    private JButton button2r;
+    private JButton removeButton;
 
     private Project myProject;
     private JComponent myAnchor;
@@ -23,27 +23,27 @@ public class ProjectDebugSettingsEditorComponent implements PanelWithAnchor {
     public ProjectDebugSettingsEditorComponent(Project project) {
         myProject = project;
         myArgs = new DefaultTableModel(new String[]{"Argument"}, 0);
-        table1.setModel(myArgs);
+        execArgumentsTable.setModel(myArgs);
         addBeforeButton.addActionListener(actionEvent -> {
-            int cursorRowIndex = table1.getSelectionModel().getLeadSelectionIndex(); // TODO Anchor instead ?
+            int cursorRowIndex = execArgumentsTable.getSelectionModel().getLeadSelectionIndex(); // TODO Anchor instead ?
             if (cursorRowIndex != -1) {
-                var modelIndex = table1.convertRowIndexToModel(cursorRowIndex);
+                var modelIndex = execArgumentsTable.convertRowIndexToModel(cursorRowIndex);
                 myArgs.insertRow(modelIndex, new String[]{""});
             } else {
                 myArgs.addRow(new String[]{""});
             }
         });
         addAfterwardsButton.addActionListener(actionEvent -> {
-            int cursorRowIndex = table1.getSelectionModel().getLeadSelectionIndex(); // TODO Anchor instead ?
-            if (cursorRowIndex != -1 && cursorRowIndex < table1.getRowCount() - 1) {
-                var modelIndex = table1.convertRowIndexToModel(cursorRowIndex + 1);
+            int cursorRowIndex = execArgumentsTable.getSelectionModel().getLeadSelectionIndex(); // TODO Anchor instead ?
+            if (cursorRowIndex != -1 && cursorRowIndex < execArgumentsTable.getRowCount() - 1) {
+                var modelIndex = execArgumentsTable.convertRowIndexToModel(cursorRowIndex + 1);
                 myArgs.insertRow(modelIndex, new String[]{""});
             } else {
                 myArgs.addRow(new String[]{""});
             }
         });
-        button2r.addActionListener(actionEvent -> {
-            Arrays.stream(table1.getSelectedRows()).map(table1::convertRowIndexToModel).boxed().sorted(Comparator.reverseOrder()).forEach(myArgs::removeRow);
+        removeButton.addActionListener(actionEvent -> {
+            Arrays.stream(execArgumentsTable.getSelectedRows()).map(execArgumentsTable::convertRowIndexToModel).boxed().sorted(Comparator.reverseOrder()).forEach(myArgs::removeRow);
         });
 
     }
@@ -60,7 +60,7 @@ public class ProjectDebugSettingsEditorComponent implements PanelWithAnchor {
     }
 
     public JComponent getComponent() {
-        return p;
+        return panel;
     }
 
     public String[] getExecArguments() {
